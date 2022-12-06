176063a89b637ce8024fcbfef75b7e5bd89ce549
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-06 16:41:20 +0200

    imported linux module for kmalloc kfree

--- EX3/message_slot.c
+++ EX3/message_slot.c
@@ -10,7 +10,7 @@
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
 #include <linux/errno.h>
-#include <linuk/slab.h>
+#include <linux/slab.h>
 
 
 
