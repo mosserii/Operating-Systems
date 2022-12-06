ea1c686880ccc3e26c90943300a534580defae3f
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-06 15:02:31 +0200

    completed 90% of the functions

--- EX3/message_slot.c
+++ EX3/message_slot.c
@@ -42,13 +42,13 @@ typedef struct channel{
     unsigned int id;
     char* current_message;
     int message_length;
+    struct channel* next;
 }channel;
 
 typedef struct message_slot{
     channel* first_channel;
     channel* current_channel;
     int isSET;
-    int minor_num;
 }message_slot;
 
 /*data structure to describe individual message slots*/
@@ -79,7 +79,6 @@ static int device_open( struct inode* inode,
 
         messageSlot->first_channel = channel1;
         messageSlot->isSET = 0;
-        messageSlot->minor_num = minor;
 
         message_slots_array[minor] = messageSlot;
     }
@@ -205,16 +204,65 @@ static ssize_t device_write( struct file*       file,
 //----------------------------------------------------------------
 static long device_ioctl( struct   file* file,
                           unsigned int   ioctl_command_id,
-                          unsigned long  ioctl_param )
-{
-    // Switch according to the ioctl called
-    if( IOCTL_SET_ENC == ioctl_command_id ) {
-        // Get the parameter given to ioctl by the process
-        printk( "Invoking ioctl: setting encryption "
-                "flag to %ld\n", ioctl_param );
-        encryption_flag = ioctl_param;
+                          unsigned long  ioctl_param ){
+
+    message_slot* messageSlot;
+    channel* channel_ptr;
+    channel* prev_channel;
+    int already_exists = 0;
+
+    int minor;
+
+
+
+
+    printk("device_ioctl invoked\n");
+    if( MSG_SLOT_CHANNEL != ioctl_command_id || 0 == ioctl_param) {
+        return -EINVAL;
     }
 
+    if(file->f_inode == NULL)
+        return -EINVAL;
+    minor = iminor(file->f_inode);
+
+    if(message_slots_array[minor]==NULL){
+        return -1;/*todo check*/
+    }
+    file->private_data = (void*) message_slots_array[minor];
+    messageSlot = (message_slot*) (file->private_data);
+    channel_ptr = (channel*) (messageSlot->first_channel);
+
+    while (channel_ptr != NULL){/*todo big check here*/
+        if (channel_ptr->id == ioctl_command_id){
+            already_exists = 1;
+            break;
+        }
+        /*keep going till you find the channel is already exists or channel_ptr is NULL*/
+        prev_channel = channel_ptr;
+        channel_ptr = channel_ptr->next;
+    }
+
+    /*channel creation*/
+    if (!already_exists){
+        /*todo check if channel_ptr is the right variable to mess with*/
+        channel_ptr = (channel*) kmalloc(sizeof(channel), GFP_KERNEL);
+        if (channel_ptr == NULL){
+            printk("channel_ptr kmalloc failed\n");
+            return -1;
+        }
+        channel_ptr->id = ioctl_command_id;
+        channel_ptr->next = NULL;
+        channel_ptr->current_message = NULL;
+        channel_ptr->message_length = 0;
+        if (!messageSlot->isSET){
+            messageSlot->first_channel = channel_ptr;
+            messageSlot->isSET = 1;
+        }
+        else {/*messageSlot is already set*/
+            prev_channel->next = channel_ptr;/*adding channel_ptr to the end of the linked list*/
+        }
+    }
+    messageSlot->current_channel = channel_ptr;/*todo check, if you change the name so be careful when it was already exists*/
     return SUCCESS;
 }
 
@@ -242,19 +290,34 @@ static int __init simple_init(void){
     // Negative values signify an error
     if( rc < 0 ){
         printk("%s registraion failed for  %d\n",
-                DEVICE_FILE_NAME, MAJOR_NUM );
+               DEVICE_RANGE_NAME, MAJOR_NUM );/*todo check if it is DEVICE_FILE_NAME or file_name*/
         return rc;
     }
     printk( "Registeration is successful, major number is : %d \n", MAJOR_NUM);
 
-    return 0;
+    return SUCCESS;
 }
 
 //---------------------------------------------------------------
 static void __exit simple_cleanup(void){ /*todo functions and cleaning here*/
-    // Unregister the device
-    // Should always succeed
+    channel* channel_ptr, next_channel;
     unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
+
+    int j;
+    for (j = 0; j < MAX_NUM_OF_FILES; j++) {
+        if (message_slots_array[j] != NULL){
+            /*cleaning message slot number j*/
+            channel_ptr = message_slots_array[j]->first_channel;
+            while(channel_ptr != NULL){
+                next_channel = channel_ptr->next;
+                if (channel_ptr->current_message != NULL)
+                    kfree(channel_ptr->current_message);
+                kfree(channel_ptr);
+                channel_ptr = next_channel;
+            }
+            kfree(message_slots_array[j]);
+        }
+    }
 }
 
 //---------------------------------------------------------------
