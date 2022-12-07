eac5af499af7fdeb08f671f14b53be40076e7977
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-07 11:09:08 +0200

    added prints to debug ioctl

--- EX3/commands.sh
+++ EX3/commands.sh
@@ -8,8 +8,6 @@ sudo chmod 0777 /dev/ops2
 sudo chmod 0777 /dev/ops3
 gcc -O3 -Wall -std=c11 message_reader.c -o READER
 gcc -O3 -Wall -std=c11 message_sender.c -o SENDER
-./SENDER /dev/ops1 6 hola
-./READER /dev/ops1 6
-dmesg
+
 
 
