14926f523f1bc1f56b39617577f485e0e87df5b4
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-07 10:24:45 +0200

    bash session created

---
+++ EX3/commands.sh
@@ -0,0 +1,12 @@
+make
+sudo insmod message_slot.ko
+sudo mknod /dev/ops1 c 235 1
+sudo mknod /dev/ops2 c 235 2
+sudo mknod /dev/ops3 c 235 3
+sudo chmod 0777 /dev/ops1
+sudo chmod 0777 /dev/ops2
+sudo chmod 0777 /dev/ops3
+gcc -O3 -Wall -std=c11 message_reader.c -o READER
+gcc -O3 -Wall -std=c11 message_sender.c -o SENDER
+
+
