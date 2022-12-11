14987e7c914792f9c7f62caa7a1d5e2ca9dc9630
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-11 22:38:45 +0200

    no need of channel malloc

--- EX3/commands.sh
+++ EX3/commands.sh
@@ -8,6 +8,9 @@ sudo chmod 0777 /dev/ops2
 sudo chmod 0777 /dev/ops3
 gcc -O3 -Wall -std=c11 message_reader.c -o READER
 gcc -O3 -Wall -std=c11 message_sender.c -o SENDER
+gcc -O3 -Wall -std=c11 tester.c -o testerC
+gcc -O3 -Wall -std=c11 tester2.c -o testerC2
+
 
 
 
