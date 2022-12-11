a55c05687d78f5a6d3c5d58583f997fba8c18438
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-11 23:45:46 +0200

    bash changed

--- EX3/commands.sh
+++ EX3/commands.sh
@@ -1,3 +1,22 @@
+make
+sudo insmod message_slot.ko
+sudo mknod /dev/ops1 c 235 1
+sudo mknod /dev/ops2 c 235 2
+sudo mknod /dev/ops3 c 235 3
+sudo chmod 0777 /dev/ops1
+sudo chmod 0777 /dev/ops2
+sudo chmod 0777 /dev/ops3
+gcc -O3 -Wall -std=c11 message_reader.c -o READER
+gcc -O3 -Wall -std=c11 message_sender.c -o SENDER
+gcc -O3 -Wall -std=c11 tester.c -o testerC
+gcc -O3 -Wall -std=c11 tester2.c -o testerC2
+
+sudo mknod /dev/test0 c 235 0
+sudo mknod /dev/test1 c 235 1
+sudo chmod 0777 /dev/test0
+sudo chmod 0777 /dev/test1
+gcc -O3 -Wall -std=c11 ex3_tester.c -o tester
+./tester
 
 sudo rmmod message_slot
 sudo rm /dev/test0
@@ -16,5 +35,6 @@ sudo insmod message_slot.ko
 sudo mknod /dev/msgslot1 c 235 1
 sudo chmod o+rw /dev/msgslot1
 ./testerC2 /dev/msgslot1
+
 
 
