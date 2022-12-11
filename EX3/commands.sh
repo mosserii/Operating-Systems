ff8236dbd49a920476717ce77473128dbdc32840
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-11 22:52:45 +0200

    bash file changed

--- EX3/commands.sh
+++ EX3/commands.sh
@@ -1,12 +1,15 @@
+sudo rmmod message_slot
+sudo mknod /dev/msgslot1 c 235 1
+sudo chmod o+rw /dev/msgslot1
+
 make
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install colorama
+
+gcc -O3 -Wall -std=c11 message_sender.c message_slot.h -o message_sender.o
+gcc -O3 -Wall -std=c11 message_reader.c message_slot.h -o message_reader.o
+
 sudo insmod message_slot.ko
-sudo mknod /dev/test0 c 235 0
-sudo mknod /dev/test1 c 235 1
-sudo chmod 0777 /dev/test0
-sudo chmod 0777 /dev/test1
-gcc -O3 -Wall -std=c11 ex3_tester.c -o tester
-./tester
-
-
-
+python3 ./tester.py
 
