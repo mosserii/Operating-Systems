6841ad30081b9cd9f65d8e450984e30d9dab3aeb
Zohar Mosseri <zoharmosseri@mail.tau.ac.il>
2022-12-25 09:42:00 +0200

    added sh file

--- 08-threads/cmds.sh
+++ 08-threads/cmds.sh
@@ -1,5 +1,5 @@
-gcc -O3 -Wall -std=c11 1_simple_thread.c -o ONE
-gcc -O3 -Wall -std=c11 2_return.c -o TWO
-gcc -O3 -Wall -std=c11 3_intermediate_thread.c -o THREE
-gcc -O3 -Wall -std=c11 4_hard_thread.c -o FOUR
-gcc -O3 -Wall -std=c11 5_expert_thread.c -o FIVE
+gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -lpthread 1_simple_thread.c -o ONE
+gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -lpthread 2_return.c -o TWO
+gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -lpthread 3_intermediate_thread.c -o THREE
+gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -lpthread 4_hard_thread.c -o FOUR
+gcc -O3 -D_POSIX_C_SOURCE=200809 -Wall -std=c11 -lpthread 5_expert_thread.c -o FIVE
